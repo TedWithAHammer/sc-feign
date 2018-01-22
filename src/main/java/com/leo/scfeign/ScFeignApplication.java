efdf18d6ea2e2433c8d78aa291b2243f4a558af6
wangly1004 <wangliyingwad@163.com>
2018-01-22 10:17:25 +0800

    1.feign init

---
+++ src/main/java/com/leo/scfeign/ScFeignApplication.java
@@ -0,0 +1,12 @@
+package com.leo.scfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScFeignApplication.class, args);
+	}
+}
